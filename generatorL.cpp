ab89254cd008ec2c01cd5d2bbecb9a75e5e90c68
Browmis <bmisha85@gmail.com>
2016-11-11 16:18:21 +0100

    created class point, add new method fill labirint, add new function printm.

--- generatorL.cpp
+++ generatorL.cpp
@@ -1,7 +1,100 @@
-//массив из 4 элемнтов
+#include <iostream>
+#include <stack>
 
-//проверяем элемент из массиве в marker на соседей
+using namespace std;
 
-//если у элемента соседий нет возврашаем bool volid 0
+class Point {
+	public:
+		Point(int I, int J) 
+		{
+			i = I;
+			j = J;
+		}
 
-//если соседи есть выбираем рандомного соседа и возвращращаем point
+		int getI() {
+			return i;
+		}
+		int getJ() {
+			return j;
+		}
+
+	private:
+		int i; //Свойство
+		int j; //свойство
+};
+void fill_labirint(int **labirint, int size)
+{
+	for (int i = 0; i < size; i ++)
+	{
+		for (int j = 0; j < size; j ++)
+		{
+			if ( i % 2 != 0 && j % 2 != 0) 
+				labirint[i][j] = 0;
+			else
+				labirint[i][j] = 1; 
+		}
+	}
+}
+void print (int **labirint, int size)
+{
+	for (int i = 0; i < size; i ++)
+	{
+		if (i % 2 == 0)
+			for ( int j = 0; j < size; j ++)
+			{
+				if (labirint[i][j] != 0)
+					cout << "-";
+				else
+					cout << " ";
+			}
+		else
+			for ( int j = 0; j < size; j ++)
+			{
+				if (labirint[i][j] != 0)
+					cout << "|";
+				else
+					cout << " ";
+			}
+		cout << endl;
+
+	}
+	
+}
+
+void generator (int **labirint, int n, stack<Point *> &Stack)
+{
+
+
+
+
+}
+
+
+int main()
+{
+	int n = 3;
+	int size = n * 2 + 1;
+	int **labirint = new int* [2 * n + 1];
+	for ( int i = 0; i < (2 * n + 1); i ++)
+	{
+		labirint[i] = new int [2 * n + 1];
+	}
+	stack <Point *> Stack; 
+	Stack.push (new Point(1,1));
+	fill_labirint(labirint, size);
+	generator(labirint, n, Stack);
+	print(labirint, size);
+	return 0;
+}
+
+/*
+marker		array
+0000		1111111
+0000		1010101					
+0000		1111111
+0000		1010101
+0000		1111111	
+			1010101	
+			1111111		
+			*/	
+
